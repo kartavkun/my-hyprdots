67d937df7466d6da73c218c39f4f7a9c2914bc15
kartavkun <nikitarobezhko@yandex.ru>
2024-10-07 02:23:34 +0500

    fix install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,13 +1,12 @@
 #/bin/bash
 
-SCRIPT_DIR = "~/dotfiles/scripts/main"
+SCRIPT_DIR="$HOME/dotfiles/scripts/main"
 
-chmod +x $SCRIPT_DIR
+chmod +x $SCRIPT_DIR/*
 
-./$SCRIPT_DIR/yay.sh
-./$SCRIPT_DIR/packages.sh
-./$SCRIPT_DIR/config.sh
-./$SCRIPT_DIR/language.sh
-./$SCRIPT_DIR/browser.sh
-./$SCRIPT_DIR/zsh.sh
-./$SCRIPT_DIR/theme.sh
+$SCRIPT_DIR/packages.sh
+$SCRIPT_DIR/config.sh
+$SCRIPT_DIR/browser.sh
+$SCRIPT_DIR/zsh.sh
+$SCRIPT_DIR/theme.sh
+$SCRIPT_DIR/language.sh
