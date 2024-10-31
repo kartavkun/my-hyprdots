284fe499a0c8f23ab4d8b884d0351d279c88c31a
kartavkun <nikitarobezhko@yandex.ru>
2024-10-31 16:25:05 +0500

    add nvim .local

---
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -0,0 +1,38 @@
+return {
+  {
+    "stevearc/conform.nvim",
+    -- event = 'BufWritePre', -- uncomment for format on save
+    opts = require "configs.conform",
+  },
+
+  -- These are some examples, uncomment them if you want to see them work!
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    'IogaMaster/neocord',
+    event = "VeryLazy"
+  },
+  -- {
+  --   'nvimdev/dashboard-nvim',
+  --   event = 'VimEnter',
+  --   config = function()
+  --     require('dashboard').setup {
+  --       -- config
+  --     }
+  --   end,
+  --   dependencies = { {'nvim-tree/nvim-web-devicons'}}
+  -- }
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
+}
---
+++ scripts/main/nvim.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+
+cp ~/dotfiles/nvim/.config ~/.config/
+cp ~/dotfiles/nvim/.local ~/.local/share
