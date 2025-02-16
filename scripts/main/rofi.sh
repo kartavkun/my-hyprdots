707ff542b81fa12ae5aebc2ec787f338decb6fc2
kartavkun <nikitarobezhko@yandex.ru>
2025-02-17 02:27:17 +0500

    I forgot to add rofi xdd

--- scripts/main/rofi.sh
+++ scripts/main/rofi.sh
@@ -28,31 +28,9 @@ install_fonts() {
   fc-cache
 }
 
-# Install Themes
-install_themes() {
-  if [[ -d "$ROFI_DIR" ]]; then
-    echo -e ${BPurple}"[*] Creating a backup of your rofi configs..." ${Color_Off}
-    mv "$ROFI_DIR" "${ROFI_DIR}.${USER}"
-  fi
-  echo -e ${BBlue}"[*] Installing rofi configs..." ${Color_Off}
-  {
-    mkdir -p "$ROFI_DIR"
-    cp -rf $DIR/files/* "$ROFI_DIR"
-  }
-
-  if [[ -f "$ROFI_DIR/config.rasi" ]]; then
-    echo -e ${BGreen}"[*] Successfully Installed.\n" ${Color_Off}
-    exit 0
-  else
-    echo -e ${BRed}"[!] Failed to install.\n" ${Color_Off}
-    exit 1
-  fi
-}
-
 # Main
 main() {
   install_fonts
-  install_themes
 }
 
 main
