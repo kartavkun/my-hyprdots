85fbcfaf9d115913dc185e2df63368743ef1c5ad
kartavkun <nikitarobezhko@yandex.ru>
2025-03-14 21:55:53 +0500

    fix rofi and add otd fix

---
+++ configs/rofi/rofi/launchers/type-6/clipboard.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+## Путь к теме оформления
+dir="$HOME/.config/rofi/launchers/type-6"
+theme='style-11'
+
+## Команда для отображения истории буфера обмена
+cliphist list | rofi -dmenu -theme ${dir}/${theme}.rasi -p "Clipboard History" | cliphist decode | wl-copy
